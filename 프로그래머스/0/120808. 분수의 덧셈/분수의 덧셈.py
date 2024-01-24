39174d48cb43453eea0391d02da4b4ba4c1229c1
sonny0430 <157447884+sonny0430@users.noreply.github.com>
2024-01-25 01:05:46 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.43 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -0,0 +1,14 @@
+def solution(numer1, denom1, numer2, denom2):
+    mul_numer = numer1*denom2 + numer2*denom1
+    mul_denom = denom1*denom2
+
+    common_mulple = []
+
+    for i in range(1, min(mul_numer, mul_denom)+1):
+        if mul_numer%i == 0 and mul_denom%i == 0:
+            common_mulple.append(i)        
+        else:
+            i = i+1
+
+    return [int(mul_numer/max(common_mulple)), int(mul_denom/max(common_mulple))]
+        
