03a2e8cb317b96a093e2105a3102cd7c65cd160a
sonny0430 <157447884+sonny0430@users.noreply.github.com>
2024-01-27 00:04:00 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -4,7 +4,6 @@ def solution(array):
     print(no_duplication_array)
 
     for i in range(0, len(no_duplication_array)):
-        print(i)
         count_list.append(array.count(no_duplication_array[i]))
     
     if count_list.count(max(count_list)) == 1:
