5e5598532d0f0ec364f6b285f532e5fd094ec415
sonny0430 <157447884+sonny0430@users.noreply.github.com>
2024-01-26 22:56:00 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.py
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.py
@@ -1,4 +1,8 @@
-def solution(num1, num2):
-    if num1 == num2:
-       return 1
-    return -1
+def solution(num1, num2):    
+    return (1 if num1 == num2 else -1)
+solution(2, 3)
+
+# def solution(num1, num2):
+#     if num1 == num2:
+#        return 1
+#     return -1
