dfe816ad80ca899a926007695c6f0836593a1352
sonny0430 <157447884+sonny0430@users.noreply.github.com>
2024-01-29 22:43:45 +0900

    [level 0] Title: 배열의 유사도, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
@@ -1,7 +1,10 @@
 def solution(s1, s2):
-    count = 0
-    for i in range(len(s2)):
-        if s2[i] in s1:
-            count += 1
-    return count
-    
+    return len(s1 + s2) - len(set(s1 + s2))
+
+
+# def solution(s1, s2):
+#     count = 0
+#     for i in range(len(s2)):
+#         if s2[i] in s1:
+#             count += 1
+#     return count
