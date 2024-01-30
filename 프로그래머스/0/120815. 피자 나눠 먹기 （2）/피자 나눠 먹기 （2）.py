955d18f0c91f95e24b0cee861ddca41487f27b2d
sonny0430 <157447884+sonny0430@users.noreply.github.com>
2024-01-30 21:09:10 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
@@ -0,0 +1,11 @@
+def solution(n):
+    a = n
+    for i in range(2, max(n, 6)+1):
+        if (a % 6 == 0):
+            break
+        else:
+            a = n * i
+        print(i)
+    return a // 6
+
+    # 최소공배수
