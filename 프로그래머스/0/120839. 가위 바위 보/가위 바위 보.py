6b02003e187db1c9f5ea2d9c91c2b0688eb2e367
sonny0430 <157447884+sonny0430@users.noreply.github.com>
2024-01-31 23:22:29 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -8,3 +8,9 @@ def solution(rsp):
         else:
             temp_list.append('2')
     return ('').join(temp_list)
+   
+                     
+# 다른 사람 코드 참고 // ++ 파이썬 문자열 덧셈 가능 (리스트 왜씀)
+# def solution(rsp):
+#     d = {'0':'5','2':'0','5':'2'}
+#     return ''.join(d[i] for i in rsp)
