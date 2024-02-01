7ee6cc3511cd5bb6ed549e52466487d111f05c61
sonny0430 <157447884+sonny0430@users.noreply.github.com>
2024-02-01 17:51:50 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.17 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -1,7 +1,7 @@
 def solution(my_string):
     result = ''
     for i in my_string:
-        if  ord(i) >= 97:
+        if  ord(i) >= 97: # 아스키코드 변환
             result += i.upper()
         else:
             result += i.lower()
