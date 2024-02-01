d535ca57ebd4d8b0e60685840684222b990787ee
sonny0430 <157447884+sonny0430@users.noreply.github.com>
2024-02-02 00:07:54 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -3,3 +3,7 @@ def solution(my_string):
     for i in range(0, len(temp_string)):
         temp_string[i] = int(temp_string[i])
     return temp_string
+
+# 컴프리헨션과 map 함수 체크하기
+# sorted([int(c) for c in my_string if c.isdigit()])
+# sorted(map(int, filter(lambda s: s.isdigit(), my_string)))
