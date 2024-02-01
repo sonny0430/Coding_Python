9f6a198abf32f97f9dec6fb67ca37085e53f2ce8
sonny0430 <157447884+sonny0430@users.noreply.github.com>
2024-02-01 23:41:43 +0900

    [level 0] Title: 개미 군단, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.py
@@ -0,0 +1,16 @@
+def solution(hp):
+    result = 0
+    temp_hp = hp
+    for i in range(hp):
+        if temp_hp >= 5:
+            result += temp_hp // 5
+            temp_hp = temp_hp % 5
+        elif temp_hp >= 3:
+            result += temp_hp // 3
+            temp_hp = temp_hp % 3
+        elif temp_hp >= 1:
+            result += temp_hp // 1
+            temp_hp = temp_hp % 1
+        else:
+            break
+    return result
