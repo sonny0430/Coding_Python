42118c748cec076304b804129ed70d1f28715040
sonny0430 <157447884+sonny0430@users.noreply.github.com>
2024-03-14 21:55:42 +0900

    [level 0] Title: 세 개의 구분자, Time: 32.64 ms, Memory: 21.6 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
@@ -12,3 +12,8 @@ def solution(myStr):
         return ['EMPTY']
     else:
         return list(filter(lambda x: x != '', temp3))
+    
+# 참고   
+# def solution(myStr):
+#    answer = [x for x in myStr.replace('a', ' ').replace('b', ' ').replace('c', ' ').split() if x]
+#    return answer if answer else ['EMPTY']
