b151944c2055bd1302432638dbdacf5c23c0f76c
sonny0430 <157447884+sonny0430@users.noreply.github.com>
2024-03-15 22:04:51 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -0,0 +1,18 @@
+def solution(num_list):
+    result = []
+    temp = 0
+    cnt = 0
+    for i in num_list:
+        temp = i
+        for j in range(i):
+            if temp == 1:
+                result.append(cnt)
+                break
+            elif temp % 2 == 1:
+                cnt += 1
+                temp = (temp - 1) // 2
+            elif temp % 2 == 0:
+                cnt += 1
+                temp = temp // 2
+        cnt = 0
+    return sum(result)
